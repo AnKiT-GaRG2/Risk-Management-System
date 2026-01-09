e245797748ca85688d2e549e069f0b37135d0773
Shubhamkr585 <shubhamprd585@gmail.com>
2026-01-09 19:23:29 +0530

    header updated

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ const corsOptions = {
   ].filter(Boolean), // Remove undefined values
   credentials: true, // Allow cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Allow-Origin'],
 };
 
 app.use(cors(corsOptions));
